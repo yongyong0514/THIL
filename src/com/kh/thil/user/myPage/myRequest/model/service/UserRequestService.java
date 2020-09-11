489eb3594ea18a7b42b2826d8f56438c1cf68836
YONG <yongyong0514@outlook.com>
2020-09-11 09:20:23 +0900

    나의의뢰 로그인 유저 별 연결

--- src/com/kh/thil/user/myPage/myRequest/model/service/UserRequestService.java
+++ src/com/kh/thil/user/myPage/myRequest/model/service/UserRequestService.java
@@ -6,26 +6,28 @@ import static com.kh.thil.common.JDBCTemplate.getConnection;
 import java.sql.Connection;
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
+
 import com.kh.thil.common.PageInfo;
 import com.kh.thil.user.myPage.myRequest.model.dao.UserRequestDao;
 import com.kh.thil.user.myPage.myRequest.model.vo.UserRequest;
 
 public class UserRequestService {
 
-	public int userListReqCount() {
+	public int userListReqCount(HttpServletRequest request) {
 		Connection con = getConnection();
 		
-		int listCount = new UserRequestDao().userListReqCount(con);
+		int listCount = new UserRequestDao().userListReqCount(con, request);
 		
 		close(con);
 		
 		return listCount;
 	}
 
-	public ArrayList<UserRequest> userListReqWithPaging(PageInfo pi) {
+	public ArrayList<UserRequest> userListReqWithPaging(PageInfo pi, HttpServletRequest request) {
 		Connection con = getConnection();
 		
-		ArrayList<UserRequest> list = new UserRequestDao().userListReqWithPaging(con, pi);
+		ArrayList<UserRequest> list = new UserRequestDao().userListReqWithPaging(con, pi, request);
 		
 		close(con);
 		
