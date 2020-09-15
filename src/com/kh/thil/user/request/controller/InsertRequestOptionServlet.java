c21d4d374d44ea949ffdb489f075a9ae64a3ae8c
상아 <@SangA>
2020-09-16 01:53:31 +0900

    -

--- src/com/kh/thil/user/request/controller/InsertRequestOptionServlet.java
+++ src/com/kh/thil/user/request/controller/InsertRequestOptionServlet.java
@@ -1,12 +1,16 @@
 package com.kh.thil.user.request.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kh.thil.user.request.model.vo.Request;
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 /**
  * Servlet implementation class InsertRequestOptionServlet
@@ -27,8 +31,27 @@ public class InsertRequestOptionServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-	
+			String part = request.getParameter("part");
+			String broom = request.getParameter("broom");
+			String room = request.getParameter("room");
+			int size = Integer.parseInt(request.getParameter("size"));
+			String wall = request.getParameter("wall");
+			String ceil = request.getParameter("ceil");
+			String molding = request.getParameter("molding");
+			String balco = request.getParameter("balco");
+			
+			ArrayList<Request> list = new ArrayList<Request>();
+			
+			list.add(part);
+			String path = "";
+			if(list != null) {
+				response.sendRedirect(request.getContextPath() + "/selectList.bo");
+			} else {
+				path = "views/common/errorPage.jsp";
+				
+				request.setAttribute("message", "게시판 작성 실패");
+				request.getRequestDispatcher(path).forward(request, response);
+			}
 	}
 
 	/**
--- src/com/kh/thil/user/request/model/vo/Request.java
+++ src/com/kh/thil/user/request/model/vo/Request.java
@@ -1,5 +1,12 @@
 package com.kh.thil.user.request.model.vo;
 
 public class Request {
-
+	private String rno;
+	private String optNo;
+	private int optEaNum;
+	private String optEaVar;
+	private String reqName;
+	private String reqPhone;
+	private String reqAdd;
+	private String
 }
